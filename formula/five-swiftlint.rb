ee1b4e7bcc9722b3250a17ae69912afee609a54a
Tino Emer <tino.emer@five.agency>
2022-10-12 15:28:58 +0200

    Updated brew formula with new initial version data

--- formula/five-swiftlint.rb
+++ formula/five-swiftlint.rb
@@ -1,10 +1,9 @@
 class FiveSwiftlint < Formula
-    desc "Forked Tool to enforce FIVE's Swift style and conventions"
-    homepage "https://github.com/vexonius/SwiftLint"
-    url "https://github.com/vexonius/SwiftLint/releases/latest/download/five-swiftlint.tar.gz"
+    desc "Tool to enforce FIVE's Swift style and conventions"
+    homepage "https://github.com/fiveagency/Five-SwiftLint"
+    url "https://github.com/fiveagency/homebrew-five-swiftlint/releases/download/1.0.0/five-swiftlint.tar.gz"
     license "MIT"
-    sha256 "a40ed470782313c75caea689dc82240c160e9a890d4f60e7450f79ff4b828d7e"
-    head "https://github.com/vexonius/SwiftLint", branch: "feature/custom-rules"
+    sha256 "80eaffb43ee3167b9eb9fe77f1a6a599083bfae37b13d785318fdb2dbf68e85f"
     version "1.0.0"
   
     def install
