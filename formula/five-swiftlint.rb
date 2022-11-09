1bb78052a5d72299598ad9b307ff7876e549aeaf
Tino Emer <tino.emer@five.agency>
2022-11-09 19:54:24 +0100

    Fixed wrong download link

--- formula/five-swiftlint.rb
+++ formula/five-swiftlint.rb
@@ -1,10 +1,10 @@
 class FiveSwiftlint < Formula
     desc "Tool to enforce FIVE's Swift style and conventions"
     homepage "https://github.com/fiveagency/Five-SwiftLint"
-    url "https://github.com/fiveagency/homebrew-five-swiftlint/releases/download/1.0.1/five-swiftlint.tar.gz"
+    url "https://github.com/fiveagency/homebrew-five-swiftlint/releases/download/1.0.0/five-swiftlint.tar.gz"
     license "MIT"
-    sha256 "4abfb6abff82957406dcb3818e092412ae6c08ad6df2beaa4eb05f195d493e8f"
-    version "1.0.1"
+    sha256 "80eaffb43ee3167b9eb9fe77f1a6a599083bfae37b13d785318fdb2dbf68e85f"
+    version "1.0.0"
   
     def install
         bin.install "five-swiftlint"
